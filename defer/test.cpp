bcb699386857cfe27b0d3a829d7ff508ab78e62c
Ionuț Leonte <ionut.leonte@gmail.com>
2018-02-06 03:27:34 +0200

    More tests

--- defer/test.cpp
+++ defer/test.cpp
@@ -1,14 +1,49 @@
 #include <cstdio>
+#include <cstdlib>
 #include "defer.hpp"
+
+#define out(fmt, ...) std::printf("[%3d] " fmt "\n", __LINE__, ##__VA_ARGS__)
+
+struct S_type {
+    int x = 1;
+    int y = 2;
+    int z = 3;
+};
 
 int main()
 {
-    defer { std::printf("Done with program\n"); };
+    void *p = std::malloc(1024);
+    [[gnu::unused]] long l = 0x0102030405l;
+    [[gnu::unused]] S_type s;
+
+    defer {
+        out("> p = %p", p);
+        out("> s = {%d, %d, %d}", s.x, s.y, s.z);
+        out("> Done with program");
+    };
+
+    defer {
+        out("> SECOND top-level defer");
+    };
+
+    out("%zd", sizeof(_deferhack_val_for_line_19));
 
     {
-        defer { std::printf("Done with scope\n"); };
-        std::printf("In scope\n");
+        defer {
+            std::free(p);
+            p = nullptr;
+            out(">>> l = %lx", l);
+            out(">>> Done with scope");
+        };
+
+        out(">>");
+        out(">> In scope");
+        out(">>");
     }
+
+    out("");
+    out("Main program");
+    out("");
 
     return 0;
 }
